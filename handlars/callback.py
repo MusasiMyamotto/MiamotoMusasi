e595290838d2cc25225712bb5b97b64d4d816b37
MusasiMyamotto <urmatsh0608@gmail.com>
2022-12-14 22:28:27 +0600

    hw2

--- handlars/callback.py
+++ handlars/callback.py
@@ -25,7 +25,7 @@ async def quiz_2(call: types.CallbackQuery):
         type='quiz',
         correct_option_id=2,
         explanation="по идее это сложно",
-        open_period=15,
+        #open_period=15,
         reply_markup=markup
     )
 
@@ -56,7 +56,7 @@ async def quiz_3(call: types.CallbackQuery):
         type='quiz',
         correct_option_id=0,
         explanation="",
-        open_period=15,
+        #open_period=15,
         reply_markup=markup
     )
 
@@ -86,7 +86,7 @@ async def quiz_4(call: types.CallbackQuery):
         type='quiz',
         correct_option_id=4,
         explanation="",
-        open_period=15,
+        #open_period=15,
         reply_markup=markup,
     )
 
@@ -116,7 +116,7 @@ async def quiz_5(call: types.CallbackQuery):
         type='quiz',
         correct_option_id=3,
         explanation="",
-        open_period=15,
+        #open_period=15,
         reply_markup=markup,
     )
 
@@ -147,15 +147,15 @@ async def quiz_6(call: types.CallbackQuery):
         type='quiz',
         correct_option_id=5,
         explanation="",
-        open_period=15,
+        #open_period=15,
         reply_markup=markup,
     )
 
 
 async def quiz_7(call: types.CallbackQuery):
     markup = InlineKeyboardMarkup()
-    button_call_1 = InlineKeyboardButton("NEXT 7", callback_data="button_call_7")
-    #markup.add(button_call_1)
+    button_call_1 = InlineKeyboardButton("NEXT 8", callback_data="button_call_8")
+    markup.add(button_call_1)
 
     question = "В каком фильме играли эти актёры"
     answers = [
@@ -178,9 +178,127 @@ async def quiz_7(call: types.CallbackQuery):
         type='quiz',
         correct_option_id=1,
         explanation="",
-        open_period=15,
-        #reply_markup=markup,
+        #open_period=15,
+        reply_markup=markup,
     )
+
+
+async def quiz_8(call: types.CallbackQuery):
+    markup = InlineKeyboardMarkup()
+    button_call_1 = InlineKeyboardButton("NEXT 9", callback_data="button_call_9")
+    markup.add(button_call_1)
+
+    question = "Как думаешь мне нравится этот персонаж"
+    answers = [
+        'ДА',
+        'НЕТ',
+    ]
+
+    photo = open("media/photo_2022-12-14_22-06-23.jpg", "rb")
+    await bot.send_photo(call.from_user.id, photo=photo)
+
+    await bot.send_poll(
+        chat_id=call.from_user.id,
+        question=question,
+        options=answers,
+        is_anonymous=False,
+        type='quiz',
+        correct_option_id=0,
+        explanation="",
+        #open_period=15,
+        reply_markup=markup,
+    )
+
+async def quiz_9(call: types.CallbackQuery):
+    markup = InlineKeyboardMarkup()
+    button_call_1 = InlineKeyboardButton("NEXT 10", callback_data="button_call_10")
+    markup.add(button_call_1)
+
+    question = "Расстояние от земли до Солнца"
+    answers = [
+        'без понятия',
+        'лям км',
+        '149 597 870 700 метров',
+        '150 345 788 432 метров',
+        '567 234 789 123 метров',
+        'Ты шо дурак от куда мне знать',
+    ]
+
+    photo = open("media/Без названия (5).jpeg", "rb")
+    await bot.send_photo(call.from_user.id, photo=photo)
+
+    await bot.send_poll(
+        chat_id=call.from_user.id,
+        question=question,
+        options=answers,
+        is_anonymous=False,
+        type='quiz',
+        correct_option_id=2,
+        explanation="",
+        #open_period=15,
+        reply_markup=markup,
+    )
+
+
+async def quiz_10(call: types.CallbackQuery):
+    markup = InlineKeyboardMarkup()
+    button_call_1 = InlineKeyboardButton("NEXT 11", callback_data="button_call_11")
+    markup.add(button_call_1)
+
+    question = "Какой самый лучший язык программирования?"
+    answers = [
+        'PHP',
+        'Java',
+        'JS',
+        'Python',
+        'Ruby',
+        'C#',
+    ]
+
+    await bot.send_poll(
+        chat_id=call.from_user.id,
+        question=question,
+        options=answers,
+        is_anonymous=False,
+        type='quiz',
+        correct_option_id=3,
+        explanation="",
+        #open_period=15,
+        reply_markup=markup,
+    )
+
+
+async def quiz_11(call: types.CallbackQuery):
+    markup = InlineKeyboardMarkup()
+    button_call_1 = InlineKeyboardButton("NEXT 12", callback_data="button_call_12")
+    markup.add(button_call_1)
+
+    question = "Кто лучший трансформер?"
+    answers = [
+        'Оптимус',
+        'Рэтчет',
+        'Мегатрон',
+        'Сэнтинал',
+        'Бамбл Би',
+        'У меня не было счастливого детства',
+    ]
+
+    photo = open("media/photo_2022-12-14_22-25-03.jpg", "rb")
+    await bot.send_photo(call.from_user.id, photo=photo)
+
+    await bot.send_poll(
+        chat_id=call.from_user.id,
+        question=question,
+        options=answers,
+        is_anonymous=False,
+        type='quiz',
+        correct_option_id=4,
+        explanation="",
+        #open_period=15,
+        reply_markup=markup,
+    )
+
+
 
 def register_handlers_callback(dp: Dispatcher):
     dp.register_callback_query_handler(quiz_2, text="button_call_1")
@@ -189,3 +307,7 @@ def register_handlers_callback(dp: Dispatcher):
     dp.register_callback_query_handler(quiz_5, text="button_call_4")
     dp.register_callback_query_handler(quiz_6, text="button_call_5")
     dp.register_callback_query_handler(quiz_7, text="button_call_6")
+    dp.register_callback_query_handler(quiz_8, text="button_call_7")
+    dp.register_callback_query_handler(quiz_9, text="button_call_8")
+    dp.register_callback_query_handler(quiz_10, text="button_call_9")
+    dp.register_callback_query_handler(quiz_11, text="button_call_10")
